7183441a89d6565f7feec98450d31291ebf0e261
Cristian Sacta <cristianoso19@gmail.com>
2023-10-25 16:05:27 -0500

    made object

--- src/main.js
+++ src/main.js
@@ -8,11 +8,35 @@ fetch("../data.json")
     console.log(data);
     data.forEach(element => {
         console.log(element.amount)
+
     });
   })
 
 function getToday(){
-  return new Date().toLocaleTimeString("en", { weekday: 'short'}).split(' ')[0].toLowerCase();
+  return new Date().toLocaleTimeString('en', { weekday: 'short'}).split(' ')[0].toLowerCase();
+
+}
+
+class stat {
+  constructor({
+    day,
+    amount,
+    cssStyle,
+    porcent
+  }){
+    this.day = day;
+    this.amount = amount;
+    this.cssStyle = cssStyle;
+    this.porcent = porcent;
+  }
+  setCssStyle(day){
+    if (day === getToday()){
+      this.cssStyle = 'bg-cyan'
+    } else {
+      this.cssStyle = 'bg-softRed'
+    }
+  }
+
 
 }
 
